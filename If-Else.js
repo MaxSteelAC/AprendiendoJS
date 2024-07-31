d1fc1f85ad4ff49d8d49f3ad4ccb81c19fa3b88e
Ingrid <mtra.ingrid.lopez@gmail.com>
2024-07-31 13:16:27 -0700

    Se actualizó el index.html a If-Else.js

---
+++ If-Else.js
@@ -0,0 +1,22 @@
+const name = "Ingrid"
+const age = 30
+const active = false
+const type = null
+
+// console.log(name)
+// console.log(age)
+// console.log(active)
+
+if (age >= 40) {
+    console.log("Esta enojada porque es mayor a 40")
+}
+else if (age == 30 && name == "Ingrid") {
+    console.log("Tiene 30 y es ingrid enojada")
+}
+else {
+    console.log("No es mayor a 30")
+}
+
+if (name.length > 5) {
+    console.log("Su nombre tiene mas de 5 caracteres")
+}
