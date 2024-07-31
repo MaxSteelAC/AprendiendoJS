cccb0f46e35d1b68040af415774acd2fbdfa9c9e
Ingrid <mtra.ingrid.lopez@gmail.com>
2024-07-31 13:08:52 -0700

    Se agregaron archivos de JS

---
+++ Poo.js
@@ -0,0 +1,31 @@
+class Animal {
+    constructor (name, type){
+        this.name = name;
+        this.type = type;
+    }
+
+    get animalType() {
+        return `Este animal se llama ${this.name} y es un ${this.type}`;
+    }
+}
+
+class Dog extends Animal{
+    constructor (name){
+        super(name)
+        this.type = "perro hembra"
+    }
+    get action() {
+        return "El perro hembra hace wof"
+    }
+} 
+
+class Cat extends Animal {
+    constructor (name){
+        super(name)
+        this.type = "gato hembra"
+    }
+    get action() {
+        return "El gato hembra araña"
+    }
+} 
+
